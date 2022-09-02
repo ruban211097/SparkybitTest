e91c0a628c11d6578245b79dea1290a3bb5aabf8
Rossman <ruban.r@brights.cloud>
2022-09-02 10:30:12 +0300

    ADD: Consumer App

---
+++ ConsumerApp/InitConsoleDI.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure;
+using Messsaging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsumerApp
+{
+    internal static class ConsoleDI
+    {
+        public static ServiceProvider Init(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddEnvironmentVariables()
+              .AddCommandLine(args)
+              .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddMessagingLayer()
+                .AddInfrastructureLayer(configuration)
+                .BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}
--- ConsumerApp/Program.cs
+++ ConsumerApp/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using Infrastructure;
+using MassTransit;
+using MediatR;
+using Messsaging.Consumers;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsumerApp
+{
+    public static class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Logger.Init();
+            var serviceProvider = ConsoleDI.Init(args);
+
+            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                cfg.ReceiveEndpoint("user-created-event", e =>
+                {
+                    e.Consumer(() => new UserCreatedConsumer(serviceProvider.GetService<IMediator>()));
+                });
+            });
+
+            await busControl.StartAsync(new CancellationToken());
+
+            try
+            {
+                Console.WriteLine("Press enter to exit");
+                await Task.Run(() => Console.ReadLine());
+            }
+            finally
+            {
+                await busControl.StopAsync();
+            }
+        }
+    }
+}
